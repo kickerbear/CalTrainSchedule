ee0e38c8c5b9c2e45732ab522bfba210d9c1d506
Cindy <kickerbear@msn.com>
2017-08-24 23:32:31 -0700

    Trying to bring in Json reader
    Decided to use native android library

--- app/src/main/java/com/crogers/caltrainschedule/ScheduleDataStore.java
+++ app/src/main/java/com/crogers/caltrainschedule/ScheduleDataStore.java
@@ -6,12 +6,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +29,11 @@ import java.util.List;
 
 public class ScheduleDataStore {
 
+    public ScheduleDataStore(){
+        //FileReader in = new FileReader("trips.json");
+        //JsonReader reader = new JsonReader(in);
+
+    }
 
     private List<String> getCities(){
         return new ArrayList<String>(Arrays.asList("San Francisco", "San Mateo"));
